246b512df0de260e8d1335ba9e4d01a09161fe35
Morad17 <morad123@hotmail.co.uk>
2022-11-09 20:00:13 +0000

    created carousel, fixed bugs stopping it moving

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MainCarousel from "../components/MainCarousel"
+
 import Navbar from '../components/global/Navbar'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <MainCarousel />
     </div>
   )
 }
